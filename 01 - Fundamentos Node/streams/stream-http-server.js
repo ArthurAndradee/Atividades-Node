39ec2b7b2d4c443bb61fdbee2c9dec45c2b1d326
arthur andrade <arthurandrad.23@gmail.com>
2024-05-24 21:03:22 -0300

    Feat: Experimented with operations

--- 01 - Fundamentos Node/streams/stream-http-server.js
+++ 01 - Fundamentos Node/streams/stream-http-server.js
@@ -14,8 +14,18 @@ class ToInverseNumberStream extends Transform {
 // req = ReadableStream
 // rep = WritableStream
 
-const server = http.createServer((req, res) => {
-    return req.pipe(new ToInverseNumberStream().pipe(res))
+const server = http.createServer(async (req, res) => {
+    const buffers = []
+
+    for await (const chunk of req) {
+        buffers.push(chunk)
+    }
+
+    const fullStreamContent = Buffer.concat(buffers).toString()
+
+    console.log(fullStreamContent)
+
+    return res.end(fullStreamContent)
 })
 
 server.listen(3334)
